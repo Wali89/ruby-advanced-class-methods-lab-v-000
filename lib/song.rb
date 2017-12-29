c2f65fcdb7c287b0375f2441b993ace6d21d7394
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-29 04:34:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,31 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+     song = Song.new
+     song.save
+     song
+   end
+ 
+   def self.new_by_name(song_name)
+     song = self.new
+     song.name = song_name
+     song
+   end
+ 
+   def self.create_by_name(song_name)
+     song = self.create
+     song.name = song_name
+     song
+   end
+ 
+   def self.find_by_name(song_name)
+     self.all.detect{|s| s.name == song_name}
+   end
+ 
+   def self.find_or_create_by_name(song_name)
+     self.find_by_name(song_name) || self.create_by_name(song_name)
+   end
+   
 end
